c9fb8c4a7862019558a5be7ce79bd2c37c7da4c6
hayato <ha8to.f513@gmail.com>
2022-12-12 14:14:18 +0000

    いいね機能の仕様変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,26 +1,38 @@
 class FavoritesController < ApplicationController
   before_action :post_params
   def create
-    if current_user.id != @post.user_id
-      @favorite = Favorite.create(user_id: current_user.id, post_id: @post.id)
+
+    # 投稿したユーザーはいいねできない
+    # if current_user.id != @post.user_id
+    #   @favorite = Favorite.create(user_id: current_user.id, post_id: @post.id)
       
-    else
-      redirect_to request.referrer
+    # else
+
+    #   redirect_to request.referrer
       
-    end
-    # redirect_to posts_path
+    # end
+
+    # 投稿したユーザーもいいねできる
+    @favorite = Favorite.create(user_id: current_user.id, post_id: @post.id)
   end
 
   def destroy
-    if current_user.id != @post.user_id
-      @favorite = Favorite.find_by(user_id: current_user.id, post_id: @post.id)
-      @favorite.destroy
 
-    else
-      redirect_to request.referrer
+    # 投稿したユーザーはいいねできない
+
+    # if current_user.id != @post.user_id
+    #   @favorite = Favorite.find_by(user_id: current_user.id, post_id: @post.id)
+    #   @favorite.destroy
+
+    # else
+    #   redirect_to request.referrer
       
-    end
-    # redirect_to posts_path
+    # end
+
+
+    # 投稿したユーザーもいいねできる
+    @favorite = Favorite.find_by(user_id: current_user.id, post_id: @post.id)
+    @favorite.destroy
     
   end
 
